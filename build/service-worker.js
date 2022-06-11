85439006a0e01eaf84845238a600a45fe9b42bc4
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2022-06-10 21:24:08 -0700

    Revert "automatic deployment"
    This reverts commit 75ceec5722749ecfef957281d30c7641a5d7d9c8.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f37f9a6482cd5307a515c614228a1d0.js"
+  "/cmawebsite/precache-manifest.67c5b25edf4d1ab37879a61b9f8aa1d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cmawebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
