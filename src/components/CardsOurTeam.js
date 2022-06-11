3a9a8935474ec5b098c5bb2ab7718a1cf112d517
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2022-06-10 21:24:20 -0700

    Revert "Update CardsOurTeam.js"
    This reverts commit 73d2415454f570cd18e2ed25b2c311a96b3418e0.

--- src/components/CardsOurTeam.js
+++ src/components/CardsOurTeam.js
@@ -11,12 +11,6 @@ function CardsOurTeam() {
           <ul className='cards__items'>
             <CardItem
               src='images/icon.png'
-              text='Sandeep is a 2x AIME Qualifier and the Executive Director'
-              label='Sandeep Karjala - Co-Founder'
-              path='/our-team'
-            />
-            <CardItem
-              src='images/icon.png'
               text='Pranay is a 2x AIME Qualifier and the Director of Operations'
               label='Pranay Boreddy - Co-Founder'
               path='/our-team'
@@ -25,6 +19,12 @@ function CardsOurTeam() {
               src='images/icon.png'
               text='Yanzhe is a 2x AIME Qualifier and the Managing Director'
               label='Yanzhe Feng - Co-Founder'
+              path='/our-team'
+            />
+            <CardItem
+              src='images/icon.png'
+              text='Sandeep is a 2x AIME Qualifier and the Executive Director'
+              label='Sandeep Karjala - Co-Founder'
               path='/our-team'
             />
           </ul>
